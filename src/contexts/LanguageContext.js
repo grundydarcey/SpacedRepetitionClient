a06f5d8a5dcdea3de25b189c81bca3b619373f68
Beyonce Knowles <you@example.com>
2021-03-10 17:48:38 -0500

    words appearing

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 const LanguageContext = React.createContext({
   language: {},
+  words: [],
+  error: null,
   setLanguage: () => {},
+  setError: () => {},
+  clearError: () => {},
+  clearLanguage: () => {},
+  setWords: () => {},
+  clearWords: () => {},
 })
 
 export default LanguageContext;
@@ -10,18 +17,48 @@ export default LanguageContext;
 export class LanguageProvider extends Component {
   state = {
     language: {},
-    setLanguage: () => {},
-  }
+    words: [],
+    error: null,
+  };
+
+  setError = (error) => {
+    console.error(error);
+    this.setState({ error });
+  };
+
+  clearError = () => {
+    this.setState({ error: null });
+  };
+
 
   setLanguage = (language) => {
     this.setState({ language })
   }
 
+  clearLanguage = () => {
+    this.setState({ language: {} });
+  };
+
+  setWords = (words) => {
+    this.setState({ words })
+  };
+
+  clearWords = () => {
+    this.setState({ words: [] });
+  };
+
   render() {
     const value = {
       language: this.state.language,
-      setLanguage: this.state.setLanguage,
-    }
+      error: this.state.error,
+      words: this.state.words,
+      setError: this.setError,
+      setWords: this.setWords,
+      setLanguage: this.setLanguage,
+      clearError: this.clearError,
+      clearLanguage: this.clearLanguage,
+      clearWords: this.clearWords,
+    };
     return (
       <LanguageContext.Provider value={value}>
         {this.props.children}
