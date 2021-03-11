d6db874adbde67b2f1a9a024bd85a05e5589ca59
Beyonce Knowles <you@example.com>
2021-03-11 15:40:36 -0500

    show words

---
+++ src/components/IndividualWords/IndividualWords.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './IndividualWords.css';
+
+class IndividualWords extends Component {
+  static contextType = ;
+  render() {
+    return (
+        
+    )
+  }
+}
