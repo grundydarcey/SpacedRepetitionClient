3173e31a50c78df4ebcdc63713d01b0071dd47c2
Beyonce Knowles <you@example.com>
2021-03-11 16:26:37 -0500

    rs

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react';
 import './LearningRoute.css';
+import LearnContext from '../../contexts/LearnContext';
+import LanguageApiService from '../../services/language-api-service';
 
 class LearningRoute extends Component {
+  static contextType = LearnContext;
+
+  state = {
+    guess: '',
+  };
+
+  componentDidMount() {
+    LanguageApiService.getHead()
+      .then((data) => {
+        if (!data) {
+          console.error(data);
+          throw new Error('Next word not found');
+        }
+        this.context.setNextWord(data.nextWord);
+        this.context.setTotalScore(data.totalScore);
+        this.context.setWordCorrectCount(data.wordCorrectCount);
+        this.context.setWordIncorrectCount(data.wordIncorrectCount);
+      })
+      .catch(this.context.setError);
+  }
+
+  handleOnChange = (e) => {
+    const guess = e.target.value;
+    this.setState({ guess: guess });
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const guess = this.state.guess;
+    LanguageApiService.submitGuess(guess).then((res) => {
+      this.context.setPrevWord(this.context.nextWord);
+      this.context.clearError();
+      this.context.setTotalScore(res.totalScore);
+
+
+      
+    })
+  }
+
   render() {
     const arr = [1, 2]
     const correctOrIncorrect = (arr.length === 2) ? (
@@ -16,7 +57,7 @@ class LearningRoute extends Component {
 
     return (
       <section className='learn'>
-        <h2>Translate the word: (word to translate)</h2>
+        <h2>Translate the word: </h2>
         <form>
           <legend>Guess below</legend>
           <fieldset>
@@ -34,4 +75,4 @@ class LearningRoute extends Component {
   }
 }
 
-export default LearningRoute
+export default LearningRoute;
