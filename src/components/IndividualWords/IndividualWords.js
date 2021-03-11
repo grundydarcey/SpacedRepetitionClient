5a8db095ee8456f9824c21c0c2535729d8130de0
Beyonce Knowles <you@example.com>
2021-03-11 16:13:54 -0500

    title switch

--- src/components/IndividualWords/IndividualWords.js
+++ src/components/IndividualWords/IndividualWords.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import './IndividualWords.css';
 
-class IndividualWords extends Component {
-  static contextType = ;
+export default class IndividualWords extends Component {
+  static defaultProps = {
+    words: [],
+  };
+
+  renderWords = () => {
+    if (this.props.words.length === 0) {
+      return <p>Practice words not found</p>; 
+    }
+    return (
+      <ul className='practiceWords'>
+        {this.props.words.map((word) => {
+          return (
+            <li className='practiceWordCard' key={word.id}>
+              <h4 className='practiceWord'>{word.original}</h4>
+              <p className='pwcorrect'>
+                Correct Answer Count: {word.correct_count}
+              </p>
+              <p className='pwInCorrect'>
+                Incorrect Answer Count: {word.incorrect_count}
+              </p>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
   render() {
     return (
-        
+      <React.Fragment>{this.renderWords()}</React.Fragment>
     )
   }
 }
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -3,6 +3,7 @@ import './DashboardRoute.css';
 import { Link } from 'react-router-dom';
 import LanguageApiService from '../../services/language-api-service';
 import LanguageContext from '../../contexts/LanguageContext';
+import IndividualWords from '../../components/IndividualWords/IndividualWords';
 
 class DashboardRoute extends Component {
   static contextType = LanguageContext;
@@ -23,8 +24,10 @@ class DashboardRoute extends Component {
         <h2>{this.context.language.name}</h2>
         <Link to='/learn'>Start Practicing</Link>
         <h3>Words to Practice</h3>
-        <p>(List showing each word to practice with a count for the number of times the user guessed it in/correctly.)</p><br /><br /><br />
+        <p>(List showing each word to practice with a count for the number of times the user guessed it in/correctly.)</p>
         <p>Total Correct Answers: {this.context.language.total_score}</p>
+        <h3 className='practiceWordTitle'>Words to Practice</h3>
+        <IndividualWords words={this.context.words} />
       </section>
     );
   }
