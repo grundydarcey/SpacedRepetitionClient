e07bfe2bc9b317343399ababeeb1bbd2850435b0
Beyonce Knowles <you@example.com>
2021-03-11 17:25:56 -0500

    results implemented

---
+++ src/components/Results/results.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import LearnContext from '../../context/LearnContext';
+import './ResultCard.css';
+
+export class Results extends Component {
+  static contextType = LearnContext;
+
+  handleClick = () => {
+    this.context.setIsResultDisplayed(false);
+  };
+
+  renderMessage = () => {
+    if (this.context.isCorrect) {
+      return <h2 className='resultTitleCorrect'>You were correct! :D</h2>;
+    } else {
+      return (
+        <h2 className='resultTitleWrong'>Good try, but not quite right </h2>
+      );
+    }
+  }
+    
+  render() {
+    return (
+      <div className='feedback'>
+        {this.renderMessage()}
+        <div className='display'>
+          <p>The correct translation for <span en='it'>{this.context.prevWord}</span> was {this.context.answer} and you chose {this.context.guess}!</p>
+        </div>
+        <button className='tryAgain' onClick={this.handleClick}>
+          Try another word!
+        </button>
+      </div>
+    );
+  }
+}
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './LearningRoute.css';
 import LearnContext from '../../contexts/LearnContext';
 import LanguageApiService from '../../services/language-api-service';
-
+import Results from '../../components/Results/Results';
 class LearningRoute extends Component {
   static contextType = LearnContext;
 
@@ -37,42 +37,87 @@ class LearningRoute extends Component {
       this.context.setPrevWord(this.context.nextWord);
       this.context.clearError();
       this.context.setTotalScore(res.totalScore);
+      this.context.setWordCorrectCount(res.wordCorrectCount);
+      this.context.setWordIncorrectCount(res.wordIncorrectCount);
+      this.context.setNextWord(res.nextWord);
+      this.context.setAnswer(res.answer);
+      this.context.setGuess(guess);
+      this.context.setIsCorrect(res.isCorrect);
+      this.context.setIsResultDisplayed(true);
 
-
-      
     })
   }
 
   render() {
-    const arr = [1, 2]
-    const correctOrIncorrect = (arr.length === 2) ? (
-      <p>Make a guess.</p>
-    ) : (
-      <section className='incorrectguessresponse'>
-        <h3>Good try, but not quite right :(</h3>
-        <p>The correct translation for --- was --- and you chose ---.</p>
-        <button type='button' className='nextWord'>Try another word!</button>
-      </section>
-    )
-
     return (
-      <section className='learn'>
-        <h2>Translate the word: </h2>
-        <form>
-          <legend>Guess below</legend>
-          <fieldset>
-          <label htmlFor='yourGuess' id='yourGuess'>What's the translation for this word?</label>
-          <input type='text' id='yourGuess'></input><br />
-          <button type='submit' className='submit'>Submit your Answer</button><br /><br />
-          {correctOrIncorrect}
-          <p>You have answered this word correctly -- times.<br />
-          You have answered this word incorrectly -- times.<br /><br />
-          Your total score is: --</p>
-          </fieldset>
-        </form>
-      </section>
-    );
+      <section className='mainpage'>
+        <section className='learn'>
+          {!this.context.isResultDisplayed ? (
+            <section className='translateForm'>
+              <section className='translateTitle'>
+                <h2>Translate the Word:</h2>
+                <span className='word'>{this.context.nextWord}</span>
+              </section>
+              <form className='translateForm' onSubmit={(e) => this.handleSubmit(e)}>
+                <label htmlFor='learninput' className='translatelabel'>
+                  What is this word in English?
+                </label>
+                <input type='text' name='learninput' id='learninput' className='input' onChange={this.handleOnChange} required></input>
+                <button type='submit' className='guessbutton'>
+                  Submit your Guess
+                </button>
+              </form>
+            </section>
+        
+          ) : (
+            <Results />
+          )}
+          <div className='score'>
+            <div className='displayScore'>
+              <p>Your total score is: {this.context.totalScore}</p>
+            </div>
+            <p>You have answered this word correctly{' '}
+              {this.context.wordCorrectCount} times.
+            </p>
+            <p>You have answered this word incorrectly{' '}
+              {this.context.wordIncorrectCount} times.
+            </p>
+          </div>  
+        </section>
+      </section>     
+    )
   }
+
+  // render() {
+  //   const arr = [1, 2]
+  //   const correctOrIncorrect = (arr.length === 2) ? (
+  //     <p>Make a guess.</p>
+  //   ) : (
+  //     <section className='incorrectguessresponse'>
+  //       <h3>Good try, but not quite right :(</h3>
+  //       <p>The correct translation for --- was --- and you chose ---.</p>
+  //       <button type='button' className='nextWord'>Try another word!</button>
+  //     </section>
+  //   )
+
+  //   return (
+  //     <section className='learn'>
+  //       <h2>Translate the word: </h2>
+  //       <form>
+  //         <legend>Guess below</legend>
+  //         <fieldset>
+  //         <label htmlFor='yourGuess' id='yourGuess'>What's the translation for this word?</label>
+  //         <input type='text' id='yourGuess'></input><br />
+  //         <button type='submit' className='submit'>Submit your Answer</button><br /><br />
+  //         {correctOrIncorrect}
+  //         <p>You have answered this word correctly -- times.<br />
+  //         You have answered this word incorrectly -- times.<br /><br />
+  //         Your total score is: --</p>
+  //         </fieldset>
+  //       </form>
+  //     </section>
+  //   );
+  // }
 }
 
 export default LearningRoute;
