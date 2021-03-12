fe1321903b94ad2a91a4d9907a9043f562c33b4c
Beyonce Knowles <you@example.com>
2021-03-12 13:21:35 -0500

    rs

--- src/services/language-api-service.js
+++ src/services/language-api-service.js
@@ -1,5 +1,5 @@
-import config from '../config';
 import TokenService from './token-service';
+import config from '../config';
 import LearnContext from '../contexts/LearnContext';
 
 const LanguageApiService = {
@@ -7,15 +7,11 @@ const LanguageApiService = {
     return fetch(`${config.API_ENDPOINT}/language`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${TokenService.getAuthToken()}`,
-       // 'Content-Type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
-    })
-    .then(res =>
-      (!res.ok) 
-        ? res.json().then(e => Promise.reject(e))
-        : res.json()
-    )
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
   },
 
   getHead() {
@@ -23,9 +19,9 @@ const LanguageApiService = {
       method: 'GET',
       headers: {
         authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'Content-Type': 'application/json',
       },
-    })
-    .then((res) =>
+    }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
@@ -39,22 +35,20 @@ const LanguageApiService = {
       method: 'POST',
       headers: {
         authorization: `Bearer ${TokenService.getAuthToken()}`,
-        'Content-Type': 'application/json',
+        'content-type': 'application/json',
       },
       body: body,
     })
-    .then((res) => {
-      console.log(body)
-      if (!res.ok) {
-        LearnContext.setError(
-          'Something went wrong, please refresh and try again.'
-        );
-        res.json().then((e) => Promise.reject(e));
-      }
-     // console.log(res.json())
-      return res.json();
-    })
-    .catch((e) => console.error(e));
+      .then((res) => {
+        if (!res.ok) {
+          LearnContext.setError(
+            'No response from server, please refresh and try again.'
+          );
+          res.json().then((e) => Promise.reject(e));
+        }
+        return res.json();
+      })
+      .catch((e) => console.error(e));
   },
 };
 
